d5d1ba202ba821dd3012d664d517c8c932177b86
AJ Jarzebiak <asjarzeb@gmail.com>
2019-03-19 00:32:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 // Write your solution in this file!
 
 const driver = {}
+
+function updateDriverWithKeyAndValue (driver, key, value)
+// {}
+// key
+// value
+
+let driverCopy = {...driver}
+driverCopy[key] = value
+return driverCopy
